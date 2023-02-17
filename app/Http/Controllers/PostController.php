2402062e83e0275bd78a5b75c36fad7c012a5ac9
Miguel cano <miguelbas@gmail.com>
2023-02-16 19:41:38 -0500

    Added Post controller and model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,82 +4,50 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $search = $request->input('search');
+        $posts = Post::when($search, function ($query, $search) {
+                return $query->where('title', 'like', "%$search%");
+            })
+            ->with(['user' => function ($query) {
+                $query->select('id', 'username');
+            }])
+            ->latest()
+            ->paginate(10);
+        return view('post.index',compact('posts'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'title' => 'required',
+            'problem' => 'required',
+            'image' => 'image|max:2048'
+        ]);
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->store('public');
+        } else {
+            $imagePath = null;
+        }
+    
+        $post = Post::create([
+            'title' => $validated['title'],
+            'problem' => $validated['problem'],
+            'image' => $imagePath,
+            'user_id' => Auth::id()
+        ]);
+        
+        return redirect('/')->with('status', 'Post created successfully');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
     public function show(Post $post)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Post $post)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Post $post)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Post $post)
-    {
-        //
+        $responses = $post->responses()->latest()->paginate(3);
+        return view('post.show', compact('post', 'responses'));
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'user_id',
+        'problem',
+        'image'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function responses()
+    {
+        return $this->hasMany(Response::class);
+    }
 }
